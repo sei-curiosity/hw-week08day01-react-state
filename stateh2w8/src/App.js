e5fbb1d67cc49b88ceab48eeb9f42c1eafeea6cb
lmdsultan <mdgetbetter.gmail.com>
2020-01-05 23:15:00 +0300

    H2W8

---
+++ stateh2w8/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Switcher from './components/Switcher'
+function App() {
+  return (
+    <div className="App">
+      <Switcher/>
+    </div>
+  );
+}
+
+export default App;
