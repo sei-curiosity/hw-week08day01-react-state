a1d4e06379adca561364b1f716003e74b8a4c3ad
ahmedalhindi <alhindi.ahmed@gmail.com>
2020-01-05 22:36:41 +0300

    implemented toggle switch

---
+++ reactapp/src/Switch.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Switch extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            toggle: false,
+         }
+    }
+    toggleButton=()=>{
+        const {toggle} = this.state
+        this.setState({toggle: !toggle})
+    }
+    render() { 
+        let onOrOff="off"
+        let s = "on"
+        if(this.state.toggle){
+            onOrOff="on"
+            s = "off"
+        } else {
+            onOrOff="off"
+            s = "on"
+        }
+        return ( 
+            <React.Fragment>
+                <p><b>Switch is { onOrOff }</b></p>
+                <button onClick={this.toggleButton}>{ s }</button>
+            </React.Fragment>
+         );
+    }
+}
+ 
+export default Switch;
