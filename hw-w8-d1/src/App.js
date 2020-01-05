5948035b9e382cee5554b6ed603444b16a650723
Mohammed Al-Mohana <m.mohana2022@gmail.com>
2020-01-05 19:05:14 +0300

    Done

---
+++ hw-w8-d1/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Switcher from '../src/components/Switcher';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Switcher onOrOff="ON" />
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ hw-w8-d1/src/components/Switcher.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Switcher extends Component {
+    constructor(props){
+    super(props)
+    this.state = { toggle: false }
+
+    }
+    toggleButton = ()=>{
+        const { toggle } = this.state
+        this.setState ({
+            toggle: !toggle
+        })
+    }
+    render() { 
+        let {onOrOff} = this.props
+        const { toggle } = this.state
+
+        if(toggle) {
+            onOrOff = "ON" 
+        } else {
+            onOrOff = "OFF"
+        }
+        return ( 
+            <div>
+                <React.Fragment>
+        <p><b>Switch is {onOrOff}</b></p>
+        <button onClick={this.toggleButton}>{ onOrOff }</button>
+    </React.Fragment>
+            </div>
+         );
+    }
+}
+
+export default Switcher;
