6fdc993bc6e757356d403efb75da25131fc0b637
roush55 <ranaabumelha@gmail.com>
2020-01-06 20:28:42 +0300

    update

--- switcher-app/src/components/State.js
+++ switcher-app/src/components/State.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
 
 class  State extends Component {
+    
     constructor(props){
         super(props)
-          this.state = { 
-              toggle: false }
-    }  
-       toggleButton=()=>{
-        const {onOrOff}=this.state
-        this.setState({
-            onOrOff: !onOrOff
-        })
-    }
+        this.state = { toggle: false }
+    
+        }
+       
+     
+        toggleButton = ()=>{
+            const { toggle } = this.state
+            this.setState ({
+                toggle: !toggle
+            })
+        }
+
+
     render() { 
-        const {onOrOff} =this.state;
-        return ( <React.Fragment>
-            <p><b>Switch is { onOrOff }</b></p>
-            <button onClick={this.toggleButton}>{ onOrOff }</button>
-     </React.Fragment> );
-    }
-}
- 
+        let {onOrOff} = this.props
+        const { toggle } = this.state
+
+        if(toggle) {
+            onOrOff = "On"
+        } else {
+            onOrOff = "off"
+        }
+
+        return ( 
+         <div>
+         <React.Fragment>
+            <p><b>Switch is  {onOrOff}</b></p>
+            <button onClick={this.toggleButton} >{onOrOff}</button>
+          </React.Fragment> 
+          </div>
+          );
+    }}
 export default State;
